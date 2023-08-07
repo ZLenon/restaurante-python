a213cef2379f494eb444db88d8f4a378e185ada5
Lenon Nascimento Cardoso <lenoninformatica@gmail.com>
2023-08-06 22:53:12 -0300

    Testes: Testando metodos da classe INGREDIENTE

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,30 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import Restriction, Ingredient
 
 
 # Req 1
 def test_ingredient():
-    pass
+    # difinindo ingredientes aleatorios da classe ingrediente
+    cheese_one = Ingredient("queijo parmesão")
+    cheese_extra = Ingredient("queijo parmesão")
+    cheese_two = Ingredient("queijo provolone")
+    cheese_three = Ingredient("queijo mussarela")
+
+    # o método mágico __hash__ funcione como esperado.
+    assert hash(cheese_one) == hash(cheese_extra)
+    assert hash(cheese_one) != hash(cheese_two)
+    assert cheese_three != cheese_two
+    assert cheese_one == cheese_extra
+    assert cheese_two.name == "queijo provolone"
+
+    # o método mágico __repr__ funcione como esperado
+    assert repr(cheese_extra) == "Ingredient('queijo parmesão')"
+
+    # Conjuntos de ingredientes
+    assert Ingredient("queijo gorgonzola").restrictions == {
+        Restriction.LACTOSE,
+        Restriction.ANIMAL_DERIVED,
+    }
+    assert Ingredient("massa de ravioli").restrictions != {
+        Restriction.ANIMAL_DERIVED,
+        Restriction.SEAFOOD,
+    }
