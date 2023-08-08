0db89d65b2f3be63b10b1487eb92835c1b7d240e
Lenon Nascimento Cardoso <lenoninformatica@gmail.com>
2023-08-07 21:40:51 -0300

    Test: Testando classe Dish e seus metodos

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -1,6 +1,37 @@
-from src.models.dish import Dish  # noqa: F401, E261, E501
+from src.models.dish import Dish
+from src.models.ingredient import Restriction, Ingredient
+import pytest
 
 
-# Req 2
+# Requisito 2
 def test_dish():
-    pass
+    pizza = Dish("pizza", 39.90)
+    fanta = Dish("fanta", 8.09)
+    queijo = Dish("gorgonzola", 198)
+    presunto = Ingredient("presunto")
+
+    assert pizza.name == "pizza"
+    assert fanta.name == "fanta"
+    assert queijo.name == "gorgonzola"
+
+    assert pizza != queijo
+    assert fanta == fanta
+
+    assert queijo.price == 198
+    assert fanta.price == 8.09
+
+    assert hash(fanta) != hash(pizza)
+    assert hash(pizza) == hash(pizza)
+
+    assert repr(queijo) == "Dish('gorgonzola', R$198.00)"
+
+    # Adiciona um ingrediente ao ao produto
+    pizza.add_ingredient_dependency(presunto, 1)
+
+    # Verifica se ingrediente está no produto
+    assert presunto in pizza.get_ingredients()
+
+    assert Restriction.ANIMAL_DERIVED in pizza.get_restrictions()
+
+    with pytest.raises(ValueError):
+        Dish("dolly", -6)
