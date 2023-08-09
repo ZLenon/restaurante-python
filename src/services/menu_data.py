8748671ea066a821406a7745cb6092c1920d4813
Lenon Nascimento Cardoso <lenoninformatica@gmail.com>
2023-08-09 20:34:18 -0300

    Feat: classe de leitura de menu

--- src/services/menu_data.py
+++ src/services/menu_data.py
@@ -1,4 +1,23 @@
-# Req 3
+from src.models.ingredient import Ingredient
+from src.models.dish import Dish
+import csv
+
+
+# Requisito 3
 class MenuData:
-    def __init__(self, source_path: str) -> None:
-        pass
+    def __init__(self, caminho: str) -> None:
+        self.pratos_menu = {}
+
+        with open(caminho, "r") as arquivo:
+            for pratos in csv.DictReader(arquivo):
+                preco_prato = pratos["dish"]
+                if preco_prato not in self.pratos_menu:
+                    self.pratos_menu[preco_prato] = Dish(
+                        preco_prato, float(pratos["price"])
+                    )
+
+                self.pratos_menu[preco_prato].add_ingredient_dependency(
+                    Ingredient(pratos["ingredient"]),
+                    int(pratos["recipe_amount"]),
+                )
+        self.dishes = set(self.pratos_menu.values())
